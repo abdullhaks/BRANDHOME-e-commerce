55b8345c094e57b7887e885abed08f930c14560e
abdullhaks <aks4u786@gmail.com>
2024-09-10 15:16:13 +0530

    update 2

--- controller/wishListCtrl.js
+++ controller/wishListCtrl.js
@@ -49,7 +49,7 @@ const loadWishList = async(req,res)=>{
            
         }
          console .log("products are "+products);
-        return  res.render("wishlist",{products,user:id,wishListNo,cartNo});
+        return  res.render("wishlistPage",{products,user:id,wishListNo,cartNo});
     }catch(error){
         console.log(error);
         return res.render("userSideErrors",{user:id,cartNo,wishListNo});
