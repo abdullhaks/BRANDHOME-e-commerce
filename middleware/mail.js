35fce9b9db5094f1e91835360f8f547cddcc9e44
abdullhaks <aks4u786@gmail.com>
2026-01-03 17:13:41 +0530

    fix: in recovery pass page

--- middleware/mail.js
+++ middleware/mail.js
@@ -1,43 +1,34 @@
-
 const nodemailer = require("nodemailer");
 
 async function sendMail(mailOptions) {
-  
-    // const transporter = nodemailer.createTransport({
-    //   service:"",
-    //   auth:{
-    //     user:"",
-    //     pass:""
-        
-    //   }
-    // });
+  // const transporter = nodemailer.createTransport({
+  //   service:"",
+  //   auth:{
+  //     user:"",
+  //     pass:""
 
+  //   }
+  // });
 
   const transporter = nodemailer.createTransport({
-  host: `${process.env.SMTP_HOST}`,
-  port: process.env.SMTP_PORT,
-  secure: false,
-  auth: {
-    user: `${process.env.SMTP_MAIL}`,
-    pass: `${process.env.SMTP_PASSWORD}`
-  },
-  tls: {
-    rejectUnauthorized: false
+    host: `${process.env.SMTP_HOST}`,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: `${process.env.SMTP_MAIL}`,
+      pass: `${process.env.SMTP_PASSWORD}`,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  try {
+    const mailResult = await transporter.sendMail(mailOptions);
+    console.log("mail successfully send..");
+  } catch (error) {
+    console.log(error);
   }
-});
+}
 
-   
-
-    try {
-      const mailResult = await transporter.sendMail(mailOptions)
-      console.log("mail successfully send..")
-      
-    } catch (error) {
-      console.log(error);
-      
-    }
-
-
-  }
-
-  module.exports = {sendMail}
+module.exports = { sendMail };
